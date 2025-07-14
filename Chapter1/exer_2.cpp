9892f8b8d368b617bdc59647ac349fbcddf22a61
for1809 <for1809@yonsei.ac.kr>
2025-07-14 11:54:35 +0900

    C Chapter1

---
+++ Chapter1/exer_2.cpp
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int main(){
+    int num1, num2;
+    num1 = 5;
+    num2 = 10;
+    printf(" %d \n", num1*num2);
+    return 0;
+}
---
+++ Chapter1/exer_5.cpp
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(){
+    int n1 = 100;
+    int n2 = 200;
+    printf("%d", n1);
+    printf("\n\n");
+    printf("%d", n2);
+    return 0;
+}
